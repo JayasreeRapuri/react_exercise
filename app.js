890038b5a28ee9e556992937071c0c66cc3692a4
Jayasree <jayasree91914@gmail.com>
2025-07-21 10:51:32 +0530

    ep1 1

---
+++ app.js
@@ -0,0 +1,14 @@
+// import React from 'React';
+// import ReactDom from 'ReactDom';
+import { createRoot } from 'react-dom/client';
+
+// // const app = () => {
+//     const h2 = React.createElement('h2',{},'This is h2 from react')
+//     const root = createRoot(document.getElementById('root'))
+//     root.render(h2)
+// // }
+
+const h2 = React.createElement('h2',{},'This is h2 from react')
+    // const root =  ReactDOM.createRoot(document.getElementById('root'))  
+   createRoot(document.getElementById('root')).render(h2)
+    // root.render(h2)
